0503aa1a69b94349e7393bcaf2e3ce0102bded86
sudo <example@gmail.com>
2024-06-11 10:47:38 +0300

    api

--- core.py
+++ core.py
@@ -3,11 +3,35 @@ from database import session, ENGINE
 from order_router import order_router
 from product_router import product_router
 
-app = FastAPI()
-app.include_router(order_router)
-app.include_router(product_router)
+from fastapi import APIRouter
+from fastapi.encoders import jsonable_encoder
+from database import session, ENGINE
+from models import Product
 
-@app.get("/")
+app = FastAPI()
+# app.include_router(order_router)
+session = session(bind=ENGINE)
+
+
+@product_router.get("/product")
+async def get_products():
+    products = session.query(Product).all()
+    context = [
+        {
+            "id": product.id,
+            "name": product.name,
+            "description": product.description,
+            "price": product.price,
+            "count": product.count,
+        }
+        for product in products
+
+    ]
+    return jsonable_encoder(context)
+
+
+
+@app.get("/api")
 async def landing():
     return {
         "message": "This is landing page"
